b7b7b88cb08733d57ecb9e6d030589463a21baf7
AlessandroCampo <alessandrocampo97@gmail.com>
2024-02-07 17:31:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+
+<template>
+    <DcHeader></DcHeader>
+</template>
+
+
+
+<script>
+
+import DcHeader from '../src/components/DcHeader.vue';
+
+export default {
+    components: {
+        DcHeader
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+
+</script>
+
+
+<style lang="scss">
+@use './styles/general.scss'
+</style>
---
+++ src/components/DcHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <div class="header-container">
+            <figure>
+                <img src="../assets/img/dc-logo.png" alt="">
+            </figure>
+            <nav>
+                <ul>
+                    <li v-for="(voice, index) in menu_voices" :key="index">
+                        <a href="#">{{ voice }}</a>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped lang="scss">
+header {
+    background-color: white;
+
+    .header-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 60%;
+        margin: 0 auto;
+    }
+}
+</style>
+
+
+<script>
+export default {
+    data() {
+        return {
+            menu_voices: ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"]
+        }
+    }
+}
+</script>
